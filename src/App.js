aa2d0e1745bf42fa9192e1eeb845c7a65b002849
Fitrul Islam <fitrulislam@Fitruls-MacBook-Pro.local>
2018-11-16 01:41:49 +0700

    initiate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style/bulma.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="is-size-1">
+        Bookinesia
+      </div>
+    );
+  }
+}
+
+export default App;
